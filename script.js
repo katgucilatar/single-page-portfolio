8cead559b77e4ac3a82fd0f970508d163b232dd2
Karmen <katgucilatar@outlook.com>
2024-02-04 23:11:32 -0500

    finished the remaining sections of the websites and added skills, recommendations, and projects, as well as add functionality to the submit button and making pop up visible

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function addRecommendation() {
   if (recommendation.value != null && recommendation.value.trim() != "") {
     console.log("New recommendation added");
     //Call showPopup here
-
+    showPopup(true);
     // Create a new 'recommendation' element and set it's value to the user's message
     var element = document.createElement("div");
     element.setAttribute("class","recommendation");
@@ -25,3 +25,5 @@ function showPopup(bool) {
     document.getElementById('popup').style.visibility = 'hidden'
   }
 }
+
+document.getElementById("recommend_btn").addEventListener("click", addRecommendation);
